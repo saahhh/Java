00442bc80f4338631c2304c55c4314b1924145d3
saahhh <143372306+saahhh@users.noreply.github.com>
2023-09-14 17:24:28 +0900

    input output

--- chap24_입출력(IO)/src/com/kh/fileIO/File_delete_Main.java
+++ chap24_입출력(IO)/src/com/kh/fileIO/File_delete_Main.java
@@ -11,15 +11,12 @@ public class File_delete_Main {
 		//파일 객체 생성
 		File fileToDelete = new File(originFile);
 		
-		//파일 삭제
+		//파일 삭제 (휴지통을 거치지 않고 영구삭제)
 		boolean deleted = fileToDelete.delete();
 			if (deleted) {
 				System.out.println("파일이 성공적으로 삭제되었습니다.");
 			} else {
 				System.out.println("파일을 삭제하는데 실패했습니다.");
 			}
-		
-
 	}
-
 }
