609009800d43b31b9ced74c2a4cb87a1a0a288cd
saahhh <143372306+saahhh@users.noreply.github.com>
2023-09-15 17:39:43 +0900

    generics

--- chap26_Generics/src/com/kh/GenericsMain/Book.java
+++ chap26_Generics/src/com/kh/GenericsMain/Book.java
@@ -2,7 +2,7 @@ package com.kh.GenericsMain;
 
 //제너릭 클래스 정의
 // T Type 의 약자 t대신 넣고싶은 자리명을 넣어도 됨
-public class Book<T> {
+public class Book<T> { //int,double과 같은 타입을 적지 않아도 되는 것이 제너릭이다.
 	T content;
 	
 	public void put(T item) {
