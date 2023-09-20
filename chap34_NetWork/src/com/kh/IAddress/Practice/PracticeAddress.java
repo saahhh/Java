6eb900d755b1c01bee795a45ab3ad809283a2072
saahhh <143372306+saahhh@users.noreply.github.com>
2023-09-20 17:20:45 +0900

    network

--- chap34_NetWork/src/com/kh/IAddress/Practice/PracticeAddress.java
+++ chap34_NetWork/src/com/kh/IAddress/Practice/PracticeAddress.java
@@ -6,8 +6,13 @@ import java.net.UnknownHostException;
 public class PracticeAddress {
 
 	public static void main(String[] args) {
+		//호스트 이름을 가져와서 객체 생성
 		try {
 			InetAddress address = InetAddress.getByName("www.geogle.com");
+			
+			//IP주소를 Byte배열로 읽기
+			//InetAdress 객체에서 IP주소를 바이트 배열로 얻음
+			//IP주소를 바이트 배열로 얻으면 각 바이트를 통해 IP주소를 다룰 수 있다.
 			byte[] ipAddress = address.getAddress();
 			System.out.println("IP Adress bytes로 얻음");
 			for(byte ip : ipAddress) {
@@ -15,8 +20,46 @@ public class PracticeAddress {
 			}
 			System.out.println();
 			
+			//도메인 명에 지정된 모든 호스트의 IP주소를 배열로 얻기
 			InetAddress[] alladdress = InetAddress.getAllByName("www.naver.com");
 			System.out.println("네이버에 지정된 모든 호스트의 IP주소를 배열로 얻음 : ");
+			for(InetAddress addr : alladdress) {
+				System.out.println(addr.getHostAddress());
+				/*향상된 for문
+				for 반복 작업을 할 때 사용되는 제어 구조
+				특정 조건이 참인 동안 또는 리스트, 배열 등의 컬렉션 요소를 하나씩 처리할 때 사용
+				코드 구조
+				for(요소의데이터타입 변수명 : ArrayList변수명) {
+					//처리 요소 코드*/
+			
+			//호스트 이름 얻기
+			String hostName = address.getHostName();
+			System.out.println("Host Name : " + hostName);
+			
+			//지역 호스트의 IP주소 얻기
+			InetAddress localHost = InetAddress.getLocalHost();
+			System.out.println("Local Host address : " + localHost.getHostAddress());
+			
+			//멀티캐스트 주소 여부 확인
+			boolean isMultiCast = address.isMulticastAddress();
+			System.out.println("멀티캐스트 주소입니까?" + isMultiCast);
+			
+			//호스트 이름을 이용해서 InetAddress 객체 생성
+			InetAddress name = InetAddress.getByName("www.daum.net");
+			System.out.println("IP주소 이름 : " + name.getHostAddress());
+			
+			//호스트의 완전한 이름 가져오기(전체 도메인 이름)
+			//호스트의 정규화된 (FQDN) 도메인의 이름을 얻음
+			//FQDN : Full Qualified Domain Name
+			String hostFullName = address.getCanonicalHostName();
+			System.out.println("풀네임 : " + hostFullName);
+			
+			//LookPack 주소 여부 확인
+			boolean isLookPack = address.isLinkLocalAddress();
+			System.out.println("룩팩인가요? : " + isLookPack);
+			
+			
+				}	
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- chap34_NetWork/src/com/kh/SocketMain/ClientMain.java
+++ chap34_NetWork/src/com/kh/SocketMain/ClientMain.java
@@ -17,12 +17,13 @@ public class ClientMain {
 		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 		
 		//서버로부터 데이터를 받기위한 출력 스트림 생성
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		
 		//키보드로부터 입력받기 위한 생성
 		BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 	
 		String userInputString;
+		System.out.println("서버와 대화를 시작합니다. 종료하려면 'exit'를 입력하세요.");
 		while((userInputString = userInput.readLine()) != null) {
 			//사용자 입력을 서버로 전송하겠습니다.
 			out.println(userInputString);
@@ -30,6 +31,11 @@ public class ClientMain {
 			//서버로부터 받은 응답 출력
 			String serverResponse = in.readLine();
 			System.out.println("서버 너의 응답은 : " + serverResponse);
+			
+			//사용자가 'exit'를 입력하면 대화 종료
+			if("exit".equalsIgnoreCase(userInputString)) {
+				break;
+			}
 		}
 		//소켓 및 입력 스트림 닫기
 		socket.close();
