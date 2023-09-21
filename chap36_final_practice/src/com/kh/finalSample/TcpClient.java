b9ad652a2e8b4ea981fb92f590f9f71b3d16f24b
saahhh <143372306+saahhh@users.noreply.github.com>
2023-09-21 17:29:42 +0900

    final practice

--- chap36_final_practice/src/com/kh/finalSample/TcpClient.java
+++ chap36_final_practice/src/com/kh/finalSample/TcpClient.java
@@ -7,6 +7,9 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+//예외를 보고 싶을 땐 트라이캐치
+//클래스안에서 해결할 땐 throws (대신강제종료의위험성이있음)
+
 
 public class TcpClient { //메소드 추가
 	public static void main(String[] args) throws Exception {
@@ -20,7 +23,10 @@ public class TcpClient { //메소드 추가
 			Socket socket = new Socket(serverIP, port);
 			System.out.println("server connect");
 			
-			//클라이언트와 통신을 위한 입 출력 스트림 설정
+			//클라이언트와 통신을 위한 입 출력 스트림 설정 
+			//BufferedReader 는 문자 입력 '스트림'에서 텍스트를 읽고, 문자를 버퍼링하여 문자, 배열 및 줄을 효율적으로 읽을 수 있도록 한다.
+			//스트림을 쓰기 때문에 inputstream,outputstream의 단어를 사용한다
+			//BufferedReader(Reader in) : 기본 크기를 사용하는 버퍼링 문자 입력 스트림을 만듭니다. 입력 버퍼.
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			OutputStream out = socket.getOutputStream();
 			
