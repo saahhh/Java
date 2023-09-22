eba7f787c3e9b743c391b0644e20f1df26fc78ff
saahhh <143372306+saahhh@users.noreply.github.com>
2023-09-22 14:18:58 +0900

    todolist practice

--- chap37_TodoList/src/chap37_TodoList/TodoListHashMap.java
+++ chap37_TodoList/src/chap37_TodoList/TodoListHashMap.java
@@ -31,13 +31,41 @@ public class TodoListHashMap {
 	public void displayTasks() {
 		System.out.println("My TodoList");
 		for(Map.Entry<Integer, String> entry : tasks.entrySet()) {
-		
+		System.out.println(entry.getKey() + " - " + entry.getValue());
 			
 		}
 	}
 	public static void main(String[] args) {
-	
+		TodoList todoList = new TodoList();
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("할일 추가 : add, 삭제 : remove, 할일보기 : list, 종료 : exit 를 입력하세요.");
+			String choice = sc.nextLine();
+			
+			if(choice.equalsIgnoreCase("add")) {
+				System.out.println("할 일을 입력하세요.");
+				String task = sc.nextLine();
+				todoList.addTask(task);
+			} else if(choice.equalsIgnoreCase("remove")) {
+				System.out.println("삭제할 일 번호 입력하기");
+				try {
+					int index = Integer.parseInt(sc.nextLine()) -1;
+					todoList.removeRemove(index);
+				} catch (Exception e) {
+					System.out.println("유효하지 않은 입력입니다.");
+				}
+			} else if (choice.equalsIgnoreCase("list")) {
+				todoList.displayTasks();
+			} else if (choice.equals("exit") | choice.equals("EXIT")) {
+				break;
+			} else {
+					System.out.println("잘못된 명령입니다.");
+			}
+		}System.out.println("어플리케이션 종료");
+		sc.close();
 	}
+	
 }
 
 	
